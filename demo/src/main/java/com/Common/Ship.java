33aece235521c0b6a16faa9b3245aa01aa627e62
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-10-04 16:51:30 +0200

    change ship

--- demo/src/main/java/com/Common/Ship.java
+++ demo/src/main/java/com/Common/Ship.java
@@ -3,23 +3,28 @@ package com.Common;
 import java.util.ArrayList;
 import java.util.Collections;
 
+enum ShipPosition {
+    HORIZONTAL,
+    VERTICAL
+}
+
 public class Ship {
     String name;
     int size;
     Position position;
-    boolean isHorizontal;
+    ShipPosition shipPosition;
     ArrayList<Boolean> hits;
 
-    public Ship(String name, int size, Position position, boolean isHorizontal) {
+    public Ship(String name, int size, Position position, ShipPosition shipPosition) {
         this.name = name;
         this.size = size;
         this.position = position;
-        this.isHorizontal = isHorizontal;
+        this.shipPosition = shipPosition;
         this.hits = new ArrayList<Boolean>(Collections.nCopies(size, false));
     }
 
     public boolean HasPosition(Position position) {
-        if (isHorizontal) {
+        if (shipPosition == ShipPosition.HORIZONTAL) {
             return this.position.x >= position.x-size && this.position.y == position.y;
         } else 
             return this.position.y >= position.y-size && this.position.x == position.x;
--- demo/src/main/java/com/Server/Server.java
+++ demo/src/main/java/com/Server/Server.java
@@ -1,10 +1,43 @@
-package com.example.Server;
+package com.Server;
 
-import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
+import org.java_websocket.server.WebSocketServer;
 
 
-public class Server {
+public class Server extends WebSocketServer {
 
-  
+  public Server(InetSocketAddress address) {
+    super(address);
+  }
+
+  @Override
+  public void onOpen(WebSocket conn, ClientHandshake handshake) {
+    System.out.println("New connection: " + conn.getRemoteSocketAddress());
+  }
+
+  @Override
+  public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+    System.out.println("Connection closed: " + conn.getRemoteSocketAddress());
+  }
+
+  @Override
+  public void onMessage(WebSocket conn, String message) {
+    System.out.println("Received message: " + message);
+  }
+
+  @Override
+  public void onError(WebSocket conn, Exception ex) {
+    System.err.println("ERROR from " + conn.getRemoteSocketAddress() + ": " + ex);
+  }
+
+  @Override
+  public void onStart() {
+    System.out.println("Server started!");
+  }
+
+  public static void main(String[] args) {
+    int port = 8080;
+    Server server = new Server(new InetSocketAddress("localhost", port));
+    server.start();
+    System.out.println("Server started on port " + port);
+  }
 }
