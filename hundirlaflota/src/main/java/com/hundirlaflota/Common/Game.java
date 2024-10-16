a4d44777935a9955155af69950fab6aee8cfd354
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-10-16 19:22:02 +0200

    ranamed game to Game

--- hundirlaflota/src/main/java/com/hundirlaflota/Common/Game.java
+++ hundirlaflota/src/main/java/com/hundirlaflota/Common/Game.java
@@ -1,5 +1,11 @@
 package com.hundirlaflota.Common;
 import java.util.ArrayList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+import java.io.IOException;
+
 
 public class Game {
     private ArrayList<String> shipsPlayer1;
@@ -27,10 +33,48 @@ public class Game {
         this.shipsPlayer2 = shipsPlayer2;
     }
 
-    public void gameActions() {
+    public static void modifyJson(String jsonFileName, String key, String newValue) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            File jsonFile = new File(jsonFileName);
+            JsonNode rootNode = objectMapper.readTree(jsonFile);
+            if (rootNode.has(key)) {
+                ((ObjectNode) rootNode).put(key, newValue);
+                objectMapper.writeValue(jsonFile, rootNode);
+                System.out.println("Se ha modificado '" + key + "' a: " + newValue);
+            } else {
+                System.out.println("La clave '" + key + "' no existe, inútil.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error! Error!: " + e.getMessage());
+        }
     }
-    public void shipAction(estado{}, int position ){
 
+
+    public static void createJson(String jsonFileName, String playerName) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode rootNode = objectMapper.createObjectNode();
+
+        for (char ch = 'a'; ch <= 'j'; ch++) {
+            for (int num = 1; num <= 10; num++) {
+                String key = ch + String.valueOf(num);
+                rootNode.put(key, "");  
+            }
+        }
+
+        rootNode.put("playerPosition", "");
+        rootNode.put("playerName", playerName);
+
+        try {
+            objectMapper.writeValue(new File(jsonFileName), rootNode);
+            System.out.println("Archivo creado: " + jsonFileName);
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo: " + e.getMessage());
+        }
+    }
+
+
+    public void gameActions() {
     }
 }
 
